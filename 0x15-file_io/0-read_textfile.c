646d8c4ec962b494fe8321c92dca58a5a36418ff
Lord-Bish <uchennabobbyfrade@yahoo.com>
2022-11-24 21:33:45 -0800

    file io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,52 +1,39 @@
 #include "main.h"
 
 /**
- *
- * main - reads a text file and prints it on POXIS stdout
- *
- * @filename - the name of the file to be read
- * @letters - the count of the read text
- *
+ * read_textfile - Read a text file and print to POSIX stdout
+ * @filename: char string of files name
+ * @letters: number of letters to read and print
+ * Return: number of letters read and printed, or 0 if error
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-   int fd;
-   char *buf;
-   ssize_t rd, wr;
-   
-   buf = malloc(sizeof(char) * letters);
-     
-      if (!buf)
-      {
-	free(buf);
-	return (0);
-      }
-   fd = open("filename", O_RDWR);
-      
-      if (!filename || fd == -1)
-      {
-	return (0);
-      }
-   
-   rd = read(fd, buf, letters);
+	int fd;
+	ssize_t rcount, wcount;
+	char *buffer;
 
-      if (rd == -1)
-      {
-	   return (0);
-      }
+	if (filename == NULL)
+		return (0);
 
-   wr = write(STDOUT_FILENO, buf, letters);
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+		return (0);
 
-      if (wr == -1 || rd != wr)
-      {
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+	rcount = read(fd, buffer, letters);
+	if (rcount == -1)
+		return (0);
 
-	   return (0);
-      }
-	
-	free(buf);
+	wcount = write(STDOUT_FILENO, buffer, rcount);
+	if (wcount == -1 || rcount != wcount)
+		return (0);
+	free(buffer);
 
-      close(fd);
-
-      return (wr);
+	close(fd);
+	return (wcount);
 }
