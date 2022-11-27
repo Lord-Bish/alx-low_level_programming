eec818112ecfa38f36e2084d08f6c91a611cefa7
Lord-Bish <uchennabobbyfrade@yahoo.com>
2022-11-26 16:10:46 -0800

    more file io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ if (!filename)
 {
 	return (-1);
 }
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 if (fd == -1)
 {
 	return (-1);
