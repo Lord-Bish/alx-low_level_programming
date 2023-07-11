b69c27c078bade380dc6016cd75ffd9786ca3433
Lord-Bish <uchennabobbyfrade@yahoo.com>
2023-07-11 00:53:47 -0700

    more file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,27 +9,32 @@
  */
 
 ssize_t read_textfile(const char *filename, size_t letters) {
+	int file_descriptor;
+	char *buffer;
+	ssize_t bytes_read;
+	ssize_t bytes_written;
+
 	if (filename == NULL)
 		return 0;
 
-    	int file_descriptor = open(filename, O_RDONLY);
+    	file_descriptor = open(filename, O_RDONLY);
     	if (file_descriptor == -1)
         	return 0;
 
-    	char *buffer = (char *)malloc(sizeof(char) * letters);
+    	buffer = (char *)malloc(sizeof(char) * letters);
     	if (buffer == NULL) {
         	close(file_descriptor);
         	return 0;
     }
 
-    	ssize_t bytes_read = read(file_descriptor, buffer, letters);
+    	bytes_read = read(file_descriptor, buffer, letters);
     	if (bytes_read == -1) {
         	close(file_descriptor);
         	free(buffer);
         	return 0;
     }
 
-    	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
     	if (bytes_written == -1 || bytes_written != bytes_read) {
         	close(file_descriptor);
         	free(buffer);
