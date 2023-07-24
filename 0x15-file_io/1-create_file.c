475863198c8d9b41dd6917d86fddbfd85f06c52e
Lord-Bish <uchennabobbyfrade@yahoo.com>
2023-07-24 04:13:01 -0700

    more files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,25 +5,28 @@
  *                                                       * Return: 1 if it success. -1 if it fails.              */
 
 int create_file(const char *filename, char *text_content) {
-    	int bytes_written;
-	int file_descriptor;
+    int fd;
+    size_t content_len;
+    ssize_t bytes_written;
 
-	if (filename == NULL)
-        	return -1;
+    if (filename == NULL)
+        return (-1);
 
-    	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    	if (file_descriptor == -1)
-        	return -1;
+    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd == -1)
+        return (-1);
 
-    	if (text_content != NULL) {
-        bytes_written = write(file_descriptor, text_content, strlen(text_content));
-        if (bytes_written == -1) {
-            close(file_descriptor);
-            return -1;
+    if (text_content != NULL) {
+        content_len = strlen(text_content);
+        bytes_written = write(fd, text_content, content_len);
+        if (bytes_written < 0) {
+            close(fd);
+            return (-1);
         }
     }
 
-    	close(file_descriptor);
-    	return 1;
+    close(fd);
+    return (1);
 }
+
 
