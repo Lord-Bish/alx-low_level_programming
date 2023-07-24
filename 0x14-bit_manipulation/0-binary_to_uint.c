66fc763e2181e7e5b52fbb6b9eeb947b56209a79
Lord-Bish <uchennabobbyfrade@yahoo.com>
2023-07-24 06:03:06 -0700

    binary file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,31 +3,24 @@
 gned int
   * @b: binary number
   * Return: the converted number or 0 if it fails
-  */                                                    unsigned int binary_to_uint(const char *b)
-{
-	unsigned int ui;
-	int len, base_two;
+  */                                                    
 
-	if (!b)
-		return (0);
+unsigned int binary_to_uint(const char *b) {
 
-	ui = 0;
+    unsigned int result;
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
 
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-	{
-		if (b[len] != '0' && b[len] != '1')
-		{
-			return (0);
-		}
+    if (b == NULL)
+        return (0);
 
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
-	}
+    result = 0;
+    while (*b != '\0') {
+        if (*b != '0' && *b != '1')
+            return (0); /* Return 0 if there is a non-binary character */
 
-	return (ui);
+        result = (result << 1) + (*b - '0');
+        b++;
+    }
+
+    return (result);
 }
