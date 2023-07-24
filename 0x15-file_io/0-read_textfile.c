ea02da0e479aa032c03b60bfc065aee305922bbc
Lord-Bish <uchennabobbyfrade@yahoo.com>
2023-07-24 00:53:12 -0700

    corrected file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,44 +7,44 @@
  *
  * Return: numbers of letters printed. It fails, returns 0.
  */
+FILE *file;
+char *buffer;
+ssize_t bytes_read;
+ssize_t bytes_written;
 
 ssize_t read_textfile(const char *filename, size_t letters) {
-	int file_descriptor;
-	char *buffer;
-	ssize_t bytes_read;
-	ssize_t bytes_written;
-
-	if (filename == NULL){
-		return (0);
-	}
-
-    	file_descriptor = open(filename, O_RDONLY);
-    	if (file_descriptor == -1){
-        	return (0);
-	}
-
-    	buffer = (char *)malloc(sizeof(char) * letters);
-    	if (buffer == NULL) {
-        	close(file_descriptor);
-        	return (0);
+    if (filename == NULL) {
+        return (0);
     }
 
-    	bytes_read = read(file_descriptor, buffer, letters);
-    	if (bytes_read == -1) {
-        	close(file_descriptor);
-        	free(buffer);
-        	return (0);
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        return (0);
     }
 
-    	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    	if (bytes_written == -1 || bytes_written != bytes_read) {
-        	close(file_descriptor);
-        	free(buffer);
-        	return (0);
+    buffer = (char *)malloc(letters + 1);
+    if (buffer == NULL) {
+        fclose(file);
+        return (0);
     }
 
-    	close(file_descriptor);
-    	free(buffer);
+    bytes_read = fread(buffer, 1, letters, file);
+    if (bytes_read <= 0) {
+        free(buffer);
+        fclose(file);
+        return (0);
+    }
 
-    	return (bytes_written);
+    buffer[bytes_read] = '\0'; /* Null-terminate the string */
+
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    free(buffer);
+    fclose(file);
+
+    if (bytes_written != bytes_read) {
+        return (0);
+    }
+
+    return (bytes_written);
 }
+
