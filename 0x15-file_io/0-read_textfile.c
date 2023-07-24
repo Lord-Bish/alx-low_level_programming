1910f3dd90ef182aa1ad021905fdfd7a747dc2a6
Lord-Bish <uchennabobbyfrade@yahoo.com>
2023-07-24 01:08:29 -0700

    corrected file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,44 +7,44 @@
  *
  * Return: numbers of letters printed. It fails, returns 0.
  */
-FILE *file;
+int file;
 char *buffer;
 ssize_t bytes_read;
 ssize_t bytes_written;
 
 ssize_t read_textfile(const char *filename, size_t letters) {
     if (filename == NULL) {
-        return (0);
+        return 0;
     }
 
-    file = fopen(filename, "r");
-    if (file == NULL) {
-        return (0);
+    file = open(filename, O_RDONLY);
+    if (file == -1) {
+        return 0;
     }
 
     buffer = (char *)malloc(letters + 1);
     if (buffer == NULL) {
-        fclose(file);
-        return (0);
+        close(file);
+        return 0;
     }
 
-    bytes_read = fread(buffer, 1, letters, file);
+    bytes_read = read(file, buffer, letters);
     if (bytes_read <= 0) {
         free(buffer);
-        fclose(file);
-        return (0);
+        close(file);
+        return 0;
     }
 
     buffer[bytes_read] = '\0'; /* Null-terminate the string */
 
     bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
     free(buffer);
-    fclose(file);
+    close(file);
 
     if (bytes_written != bytes_read) {
-        return (0);
+        return 0;
     }
 
-    return (bytes_written);
+    return bytes_written;
 }
 
